750e7fc706218e6f032357163832a24e6e225d1f
gerardo-lopez-dev-gh <gerardo.a.lopez1994@gmail.com>
2024-09-13 08:19:31 -0400

    Introducción - Concatenación

---
+++ src/Variables/Concatenacion.java
@@ -0,0 +1,11 @@
+package Variables;
+
+public class Concatenacion {
+    public static void main(String[] args) {
+        //concetenacion
+        var nombre = "Karla";
+        var apellido = "Lara";
+        var nombreCompleto = nombre + " " + apellido;
+        System.out.println("nombreCompleto: " +  nombreCompleto);
+    }
+}
